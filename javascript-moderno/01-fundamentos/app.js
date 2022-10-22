935c21b238a5cf3e2b37c5b6b6a7b0cfb952ddf6
Mariangel Yajure <mariangel.yajure@gmail.com>
2022-10-22 19:09:06 -0400

    Variables y comentarios

--- javascript-moderno/01-fundamentos/app.js
+++ javascript-moderno/01-fundamentos/app.js
@@ -1,1 +1,11 @@
-console.log(console.log('Hola Mundo'));
+
+// console.log(console.log('Hola Mundo'));
+
+let a = 10; // Variable
+var b = 10; // Ya no se usa
+const c = 10; // Constante, no se puede reasignar
+
+/**
+ * Polyfill Es un código que provee el funcionamiento de una nueva característica de javascript ES6, en versiones viejas como ES5
+ */
+
