aab8941a79d246bb26362f5cd285fced3b6c579d
Mariangel Yajure <mariangel.yajure@gmail.com>
2022-10-23 16:27:11 -0400

    Listas 3 - Python

--- python/leccion05/1.list.py
+++ python/leccion05/1.list.py
@@ -28,3 +28,34 @@ for nombre in nombres:
     print(nombre)
 else:
     print('No existen más nombres en la lista')
+
+# preguntar el largo de una lista
+print(len(nombres))
+
+# agregar un elemento
+nombres.append('Lorenzo')
+print(nombres)
+
+# insertar un elemento en un índice específico
+nombres.insert(1, 'Octavio')
+print(nombres)
+
+# remover un elemento
+nombres.remove('Octavio')
+print(nombres)
+
+# remover el último valor agregado
+nombres.pop()
+print(nombres)
+
+# remover según índice
+del nombres[0]
+print(nombres)
+
+# limpiar lista
+nombres.clear()
+print(nombres)
+
+# remover la lista por completo (de la memoria)
+del nombres
+# print(nombres)
