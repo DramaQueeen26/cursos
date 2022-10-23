93b63365d8584318ae3208a72fa421393c49922c
Mariangel Yajure <mariangel.yajure@gmail.com>
2022-10-23 15:38:28 -0400

    Listas 2 - Python

--- python/leccion05/1.list.py
+++ python/leccion05/1.list.py
@@ -9,3 +9,22 @@ print(nombres[2])
 
 # acceder a los elementos de manera inversa
 print(nombres[-1])
+
+# imprimir un rango
+print(nombres[0:2]) # sin incluir el índice 2
+
+# ir del inicio del inicio de la lista al índice sin incluirlo
+print(nombres[:3])
+
+# ir desde el índice indicado hasta el final de la lista
+print(nombres[1:])
+
+# cambiar el valor
+nombres[3] = 'Ivone'
+print(nombres)
+
+# iterar la lista
+for nombre in nombres:
+    print(nombre)
+else:
+    print('No existen más nombres en la lista')
