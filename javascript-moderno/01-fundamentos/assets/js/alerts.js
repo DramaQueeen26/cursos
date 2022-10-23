04e43cb01c60f9195e094cc2bfdb33af9e5a0183
Mariangel Yajure <mariangel.yajure@gmail.com>
2022-10-22 20:16:34 -0400

    Promp, confirm y alert

---
+++ javascript-moderno/01-fundamentos/assets/js/alerts.js
@@ -0,0 +1,13 @@
+
+
+// alert('Hola Mundo');
+
+// let nombre = prompt('¿Cuál es tu nombre', 'Sin nombre');
+
+// console.log(nombre);
+
+const seleccion = confirm('¿Está seguro de borrar esto?');
+
+console.log(seleccion);
+
+// console.log(global); // Este es para node
