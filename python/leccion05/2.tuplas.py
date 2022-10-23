a6c89b83c37bc76c6d1c11b28d658a5503283af4
Mariangel Yajure <mariangel.yajure@gmail.com>
2022-10-23 16:53:00 -0400

    Tuplas - Python

---
+++ python/leccion05/2.tuplas.py
@@ -0,0 +1,18 @@
+# una tupla es de tipo inmutable (no modificable)
+
+# definir una tupla
+frutas = ('Naranja', 'Plátano', 'Guayaba')
+print(frutas)
+
+# largo de una tupla
+print(len(frutas))
+
+# acceder a un elemento
+print(frutas[0])
+
+# navegación inversa
+print(frutas[-1])
+
+# acceder a un rango de valores
+print(frutas[0:2]) # sin incluir el último índice
+
