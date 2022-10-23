bba3794dcb57f028134309cab045e4c84d7da4b8
Mariangel Yajure <mariangel.yajure@gmail.com>
2022-10-23 15:25:17 -0400

    Listas - Python

---
+++ python/leccion05/1.list.py
@@ -0,0 +1,11 @@
+# Definir una lista
+nombres = ['Juan', 'Karla', 'Ricardo', 'María']
+
+# imprimir la lista
+print(nombres)
+
+# accediendo a los elementos
+print(nombres[2])
+
+# acceder a los elementos de manera inversa
+print(nombres[-1])
