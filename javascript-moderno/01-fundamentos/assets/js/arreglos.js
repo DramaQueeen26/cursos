c03c667dae15241347262c6baa12c5677e8c992b
Mariangel Yajure <mariangel.yajure@gmail.com>
2022-11-03 18:46:22 -0400

    Arreglos - Javascript Moderno

---
+++ javascript-moderno/01-fundamentos/assets/js/arreglos.js
@@ -0,0 +1,25 @@
+// const arr = [];
+// console.log(arr);
+
+let videoJuegos = [ 'Mario 3', 'Megaman', 'Chrono Trigger' ];
+console.log({ videoJuegos });
+
+console.log( videoJuegos[0] );
+
+let arregloCosas = [
+    true,
+    123,
+    'Mariangel',
+    1 + 2,
+    function () {
+        console.log('Entró en la función');
+    },
+    () => {
+
+    },
+    {a: 1},
+    ['X', 'Megaman', 'Zero', 'Dr. Light']
+];
+
+// console.log({ arregloCosas });
+console.log( arregloCosas[7][3] );
