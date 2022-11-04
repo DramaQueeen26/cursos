cf8b84a7b6231da1e01ad5008e447df9111caf35
Mariangel Yajure <mariangel.yajure@gmail.com>
2022-11-04 18:03:55 -0400

    Objetos literales - Javascript Moderno

--- javascript-moderno/01-fundamentos/assets/js/arreglos.js
+++ javascript-moderno/01-fundamentos/assets/js/arreglos.js
@@ -23,3 +23,38 @@ let arregloCosas = [
 
 // console.log({ arregloCosas });
 console.log( arregloCosas[7][3] );
+
+// Más detalles
+
+// Longitud de un arreglo
+console.log( arregloCosas.length );
+
+// Recorrer un arreglo
+videoJuegos.forEach( (elem, indice, arr) => {
+    console.log({ elem, indice, arr });
+});
+
+// Insertar elemento al final
+videoJuegos.push( 'F-Zero' );
+console.log( videoJuegos );
+
+// Insertar elemento al inicio
+videoJuegos.unshift( 'Fire Emblem' );
+console.log( videoJuegos );
+
+// Remover elemento al final
+let juegoBorrado = videoJuegos.pop();
+console.log( juegoBorrado );
+console.log( videoJuegos );
+
+// Remover elemento según posición
+let pos = 1;
+videoJuegos.splice(pos, 1);
+console.log( videoJuegos );
+
+// Saber el índice de un arreglo
+let marioIndex = videoJuegos.indexOf('Megaman');
+console.log( marioIndex );
+
+
+
