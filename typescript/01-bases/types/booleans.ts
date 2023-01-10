ede4bca106b03fda135d73c206934587b23bafb7
Mariangel Yajure <mariangel.yajure@gmail.com>
2023-01-09 20:38:38 -0400

    Booleanos y números

---
+++ typescript/01-bases/types/booleans.ts
@@ -0,0 +1,9 @@
+(() => {
+
+    let isSuperman = true;
+    let isBatman = true;
+
+    isSuperman = ( isBatman ) ? false : true;
+
+    console.log({ isSuperman });
+})()
---
+++ typescript/01-bases/types/numbers.js
@@ -0,0 +1,12 @@
+"use strict";
+(() => {
+    let avengers = 10;
+    console.log(avengers);
+    const villians = 20;
+    if (avengers > villians) {
+        console.log('Nos salvamos');
+    }
+    else {
+        console.log('Estamos en problemas');
+    }
+})();
