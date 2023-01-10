90845c7132f9d91057359e721f70b989c83481e7
Mariangel Yajure <mariangel.yajure@gmail.com>
2023-01-09 21:12:29 -0400

    Arrays

---
+++ typescript/01-bases/types/arrays.js
@@ -0,0 +1,6 @@
+"use strict";
+(() => {
+    const numbers = [1, 2, 3, 4, 5];
+    numbers.push(6);
+    numbers.forEach(v => console.log(v));
+})();
---
+++ typescript/01-bases/types/arrays.ts
@@ -0,0 +1,9 @@
+(() => {
+
+    const numbers: number[] = [1, 2, 3, 4, 5];
+
+    numbers.push( 6 );
+
+    numbers.forEach( v  => console.log( v ));
+
+})()
